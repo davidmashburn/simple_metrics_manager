fdf756f9c6ba9fe7ff8999927df3cfff998e55da
David N. Mashburn <david.n.mashburn@gmail.com>
2021-01-21 15:51:16 -0500

    Black formatting

--- simple_metrics_manager/storage_interface.py
+++ simple_metrics_manager/storage_interface.py
@@ -1,6 +1,6 @@
-'''Generic storage interfaces with a common api
+"""Generic storage interfaces with a common api
    So far, only file-based interfaces are implemented (.json and .npy)
-   '''
+   """
 
 import os
 import shutil
@@ -11,6 +11,7 @@ try:
     import simplejson as json
 except ImportError:
     import json
+
 
 class StorageInterface(object):
     def __init__(self):
@@ -25,9 +26,11 @@ class StorageInterface(object):
     def load(self, name):
         pass
 
+
 def _create_tmp(filepath):
-    '''Change something.ext into something.tmp.ext'''
-    return '{}.tmp{}'.format(*os.path.splitext(filepath))
+    """Change something.ext into something.tmp.ext"""
+    return "{}.tmp{}".format(*os.path.splitext(filepath))
+
 
 class FileBasedStorageInterface(StorageInterface):
     def __init__(self, file_ext, metrics_dir):
@@ -36,11 +39,11 @@ class FileBasedStorageInterface(StorageInterface):
         StorageInterface.__init__(self)
 
     def _get_filepath(self, name):
-        '''Get the filename for any given metric'''
+        """Get the filename for any given metric"""
         return os.path.join(self.metrics_dir, name + self.file_ext)
 
     def exists(self, name):
-        '''Test if a given metric already exists'''
+        """Test if a given metric already exists"""
         filepath = self._get_filepath(name)
         return os.path.exists(filepath)
 
@@ -60,26 +63,33 @@ class FileBasedStorageInterface(StorageInterface):
         self._save(tmp, data)
         shutil.move(tmp, filepath)
 
+
 # Json is a less-than-ideal storage format for binary data, but it's easy:
 def _to_flat(x):
-    return (x.tolist() if type(x) == np.ndarray else x)
+    return x.tolist() if type(x) == np.ndarray else x
+
 
 class JsonStorageInterface(FileBasedStorageInterface):
     def __init__(self, metrics_dir):
-        FileBasedStorageInterface.__init__(self, file_ext='.json', metrics_dir=metrics_dir)
+        FileBasedStorageInterface.__init__(
+            self, file_ext=".json", metrics_dir=metrics_dir
+        )
 
     def _load(self, filepath):
         with open(filepath) as fid:
             return json.load(fid)
 
     def _save(self, filepath, data):
-        with open(filepath, 'w') as fid:
+        with open(filepath, "w") as fid:
             json.dump(_to_flat(data), fid)  # Force numpy arrays to lists
+
 
 # Npy is a actually a great storage format for this:
 class NpyStorageInterface(FileBasedStorageInterface):
     def __init__(self, metrics_dir):
-        FileBasedStorageInterface.__init__(self, file_ext='.npy', metrics_dir=metrics_dir)
+        FileBasedStorageInterface.__init__(
+            self, file_ext=".npy", metrics_dir=metrics_dir
+        )
 
     def _load(self, filepath):
         return np.load(filepath)
@@ -87,39 +97,55 @@ class NpyStorageInterface(FileBasedStorageInterface):
     def _save(self, filepath, data):
         np.save(filepath, data)
 
+
 def _int_after_underscore(x):
-    return int(x.split('_')[1])
+    return int(x.split("_")[1])
+
 
 class NpzStorageInterface(FileBasedStorageInterface):
-    '''A StorageInterface that allows entries to include multiple arrays.
+    """A StorageInterface that allows entries to include multiple arrays.
 
     Can store either a single array, a list of arrays, or a dictionary of arrays.
     This uses the metric data type to route the storage type, so ensure you
     pass an array instead of a list if you mean to store an array ;)
-    '''
-    def __init__(self, metrics_dir, compress=False, allow_pickle=False, list_limit=None):
+    """
+
+    def __init__(
+        self, metrics_dir, compress=False, allow_pickle=False, list_limit=None
+    ):
         self.compress = compress
         self.allow_pickle = allow_pickle
         self.list_limit = list_limit
-        FileBasedStorageInterface.__init__(self, file_ext='.npz', metrics_dir=metrics_dir)
+        FileBasedStorageInterface.__init__(
+            self, file_ext=".npz", metrics_dir=metrics_dir
+        )
 
     def _load(self, filepath):
         val = np.load(filepath, allow_pickle=self.allow_pickle)
-        return (dict(val) if 'arr_0' not in val.files else  # Return dictionary
-                val['arr_0'] if len(val.files) == 1 else    # Return singleton
-                [val[k] for k in sorted(val.files, key=_int_after_underscore)])        # Return list
+        return (
+            dict(val)
+            if "arr_0" not in val.files
+            else val["arr_0"]  # Return dictionary
+            if len(val.files) == 1
+            else [  # Return singleton
+                val[k] for k in sorted(val.files, key=_int_after_underscore)
+            ]
+        )  # Return list
 
     def _save(self, filepath, data):
-        args, kwds = (([], data)                           # Use dictionary storage
-                      if hasattr(data, 'keys') else
-                      (data, {})                           # Use list storage
-                      if isinstance(data, (list, tuple)) else
-                      ([data], {}))                       # Use singleton storage
+        args, kwds = (
+            ([], data)  # Use dictionary storage
+            if hasattr(data, "keys")
+            else (data, {})  # Use list storage
+            if isinstance(data, (list, tuple))
+            else ([data], {})
+        )  # Use singleton storage
         if self.list_limit is not None and len(args) > self.list_limit:
-            msg = 'Data violates list length limit! Use an array instead (or adjust the limit).'
+            msg = "Data violates list length limit! Use an array instead (or adjust the limit)."
             raise TypeError(msg)
 
         _savez(filepath, args, kwds, self.compress, self.allow_pickle)
+
 
 # A storage interface could also work by storing a "document" version of a
 # number of different metrics in a single file which would cut down on file
@@ -132,5 +158,5 @@ class NpzStorageInterface(FileBasedStorageInterface):
 # And this might be a better way for some applications:
 # class SQLStorageInterface(StorageInterface)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
--- test/test_dated_cache_manager.py
+++ test/test_dated_cache_manager.py
@@ -8,48 +8,47 @@ from simple_metrics_manager import NpzStorageInterface, ParameterizedDatedCacheM
 
 
 def test_collect_1():
-    a = np.array([1,4,5])
-    
+    a = np.array([1, 4, 5])
+
     temp_dir = tempfile.mkdtemp()
     cm = ParameterizedDatedCacheManager(NpzStorageInterface(temp_dir))
 
     @cm.collect
     def sample():
         return a
-    
-    assert hasattr(cm, 'sample')
-    assert hasattr(cm.sample, 'original_function')
+
+    assert hasattr(cm, "sample")
+    assert hasattr(cm.sample, "original_function")
     assert cm.sample.params_list == ((),)
 
-    assert 'sample' in cm.functions_dict
+    assert "sample" in cm.functions_dict
 
     assert np.array_equal(cm.sample(), a)
-    assert np.array_equal(cm.cache['sample'], a)
+    assert np.array_equal(cm.cache["sample"], a)
     sample(cache=True)
 
     shutil.rmtree(temp_dir)
 
 
 def test_collect_2():
-    a = np.array([1,4,5])
+    a = np.array([1, 4, 5])
     a_2_3 = a * 2 + 3
     params_list = [[2, 3], [4, 5]]
-    
+
     temp_dir = tempfile.mkdtemp()
     cm = ParameterizedDatedCacheManager(NpzStorageInterface(temp_dir))
 
     @cm.collect(params_list=params_list)
     def sample(x, y):
         return a * x + y
-    
-    assert hasattr(cm, 'sample')
-    assert hasattr(cm.sample, 'original_function')
-    assert hasattr(cm.sample, 'params_list')
-    assert cm.parameterized_metrics[0].__name__ == 'sample'
-    assert 'sample_2_3' in cm.functions_dict
-    assert 'sample_4_5' in cm.functions_dict
-    assert 'sample' not in cm.functions_dict
 
+    assert hasattr(cm, "sample")
+    assert hasattr(cm.sample, "original_function")
+    assert hasattr(cm.sample, "params_list")
+    assert cm.parameterized_metrics[0].__name__ == "sample"
+    assert "sample_2_3" in cm.functions_dict
+    assert "sample_4_5" in cm.functions_dict
+    assert "sample" not in cm.functions_dict
 
     assert cm.sample.params_list == params_list
 
@@ -72,40 +71,42 @@ def test_collect_2():
     assert failed
 
     assert np.array_equal(cm.sample(2, 3), a_2_3)
-    assert np.array_equal(cm.cache['sample_2_3'], a_2_3)
+    assert np.array_equal(cm.cache["sample_2_3"], a_2_3)
 
     def sample_new(x, y):
         return a * x + 2 * y
-    
-    cm.functions_dict['sample_2_3'] = lambda: sample_new(2, 3)
+
+    cm.functions_dict["sample_2_3"] = lambda: sample_new(2, 3)
     a_2_3_a = sample(2, 3, cache=True)
     assert np.array_equal(a_2_3_a, a_2_3)
     a_2_3_b = sample(2, 3, cache=True, use_stored=False)
     assert not np.array_equal(a_2_3_b, a_2_3)
     assert np.array_equal(a_2_3_b, sample_new(2, 3))
-    
+
     shutil.rmtree(temp_dir)
 
+
 def test_collect_3():
-    a = np.array([1,4,5])
+    a = np.array([1, 4, 5])
     a_2_3 = a * 2 + 3
     params_list = [[2, 3], [4, 5]]
-    
+
     temp_dir = tempfile.mkdtemp()
-    cm = ParameterizedDatedCacheManager(NpzStorageInterface(temp_dir), dynamic_metric_creation=True)
+    cm = ParameterizedDatedCacheManager(
+        NpzStorageInterface(temp_dir), dynamic_metric_creation=True
+    )
 
     @cm.collect(params_list=params_list)
     def sample(x, y):
         return a * x + y
-    
-    assert hasattr(cm, 'sample')
-    assert hasattr(cm.sample, 'original_function')
-    assert hasattr(cm.sample, 'params_list')
-    assert cm.parameterized_metrics[0].__name__ == 'sample'
-    assert 'sample_2_3' in cm.functions_dict
-    assert 'sample_4_5' in cm.functions_dict
-    assert 'sample' not in cm.functions_dict
 
+    assert hasattr(cm, "sample")
+    assert hasattr(cm.sample, "original_function")
+    assert hasattr(cm.sample, "params_list")
+    assert cm.parameterized_metrics[0].__name__ == "sample"
+    assert "sample_2_3" in cm.functions_dict
+    assert "sample_4_5" in cm.functions_dict
+    assert "sample" not in cm.functions_dict
 
     assert cm.sample.params_list == params_list
 
@@ -114,13 +115,12 @@ def test_collect_3():
     assert np.array_equal(cm.sample(2, 4, cache=True), a * 2 + 4)
 
     assert np.array_equal(cm.sample(2, 3), a_2_3)
-    assert np.array_equal(cm.cache['sample_2_3'], a_2_3)
+    assert np.array_equal(cm.cache["sample_2_3"], a_2_3)
 
     shutil.rmtree(temp_dir)
 
-    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_collect_1()
     test_collect_2()
     test_collect_3()
